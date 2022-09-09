95cf7d32f5c94c1c3fce65e77c72277635bca60b
shalemzorro <shalemzorro@gmail.com>
2022-09-09 10:57:57 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,20 +9,20 @@ int main(void)
 {
 	int c = 0, i;
 
-	while (c <= '99')
+	while (c <= '9')
 	{
 		i = c;
-		while (i <= '99')
+		while (i <= '9')
 		{
 			if (i != c)
 			{
-				putchar((c / 10) + '48');
-				putchar((c % 10) + '48');
+				putchar((c / 10) + '0');
+				putchar((c % 10) + '0');
 				putchar(' ');
-				putchar((i / 10) + '48');
-				putchar((i % 10) + '48');
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 
-				if (c != '98' || i != '98')
+				if ((c != '9') || (c == '9' && i != '9'))
 				{
 					putchar(',');
 					putchar(' ');
