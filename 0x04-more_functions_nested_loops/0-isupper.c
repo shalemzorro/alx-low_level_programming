05116464670b0d65a50dbb0ef8042b67e4753aea
shalemzorro <shalemzorro@gmail.com>
2022-09-15 11:29:54 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper -  checks for uppercase character.
+ * @c: c -  Variable
+ * Return:0
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+0x04. C - More functions, more nested loops
+By: Julien Barbier
+Project will start Sep 15, 2022 4:00 AM, must end by Sep 16, 2022 4:00 AM
