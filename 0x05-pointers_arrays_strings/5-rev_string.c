c55e1eb759e3e7cce7aa897c4a4266b709251b06
shalemzorro <shalemzorro@gmail.com>
2022-09-19 11:06:15 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: this is a parameter to change
+ */
+void rev_string(char *s)
+{
+	int ini = 0, finish = 0;
+	char t;
+
+	while (s[finish] != 0)
+		finish++;
+	finish--;
+
+	while (ini < finish)
+	{
+		t = s[ini];
+		s[ini] = s[finish];
+		s[finish] = t;
+		ini++;
+		finish--;
+	}
+}
