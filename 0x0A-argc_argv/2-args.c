aeb10b9475973fa5a2deadc6cd5386cfcf689ec3
shalemzorro <shalemzorro@gmail.com>
2022-09-30 16:43:04 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments passed into main
+ * @argc: number of command line arguments
+ * @argv: array name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
