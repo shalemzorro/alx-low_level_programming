01fb0cb6f588d001ace1a4e06e854e1898cfadd0
shalemzorro <shalemzorro@gmail.com>
2022-10-24 16:54:55 -0700

    submit

--- 0-print_list.c
+++ 0-print_list.c
@@ -1,8 +1,3 @@
-/*
- * File: 0-print_list.c
- * Auth: Shalem Zorr
- */
-
 #include "lists.h"
 #include <stdio.h>
 
@@ -14,17 +9,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t s = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-		nodes++;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		s++;
 	}
 
-	return (nodes);
+	return (s);
 }
